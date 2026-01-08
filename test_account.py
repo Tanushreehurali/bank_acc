dc76fff81aa5b52f2f4b72464f7bd81858c64af3
Tanushreehurali <tanuhurali@gmail.com>
2026-01-08 16:32:27 +0530

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -1,6 +1,6 @@
 from account import account_details
 def account_details_output():
-    result account_details("24ECF123","tanu","saving",32000)
+    return account_details("24ECF123","tanu","saving",32000)
 expected=(
     "account_no=24ECF123\n"
     "account holdername=tanu\n"
