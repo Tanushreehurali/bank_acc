57070c0fa77707347dcac9b8d47f250f8eabfed4
Tanushreehurali <tanuhurali@gmail.com>
2026-01-08 15:36:38 +0530

    save

---
+++ account.py
@@ -0,0 +1,9 @@
+def account_details(account_no,account_holdername,account_type,balance):
+    return(
+        f"account_number:{account_no}\n"
+        f"account_holdername:{account_holdername}\n"
+        f"account_type:{account_type}\n"
+        f"balance:{balance}\n"
+    )
+
+
