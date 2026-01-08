c00ac7b415df882d4535abb9113ced8ba8f33527
Tanushreehurali <tanuhurali@gmail.com>
2026-01-08 16:47:12 +0530

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -2,10 +2,10 @@ from account import account_details
 def account_details_output():
     return account_details("24ECF123","tanu","saving",32000)
 expected=(
-    "account_no=24ECF123\n"
-    "account holdername=tanu\n"
-    "account_type=saving\n"
-    "balance=32000\n"
+    " acc_no= 24ECF123\n"
+    "acc_name = tanu\n"
+    "acc_type = savings\n"
+    "bal = 32000\n"
 
 )
 assert return==expected
